4f4689f870f3566c842ec51e641d02fc1901b903
the8projects <the8projects@gmail.com>
2021-06-22 19:28:22 +0700

    Add new project to practice

---
+++ CuisineVision/DataAccess.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace CuisineVision
+{
+    class DataAccess
+    {
+        public bool DbLogin(string account, string password)
+        {
+            using (MySqlConnection db = new MySqlConnection("server=localhost; port=3306; uid=root; database=cuisinevision; charset=utf8;"))
+            {
+                try
+                {
+                    db.Open();
+                    string strSql = "SELECT * FROM account WHERE AccUsername = @account AND AccPassword = @password;";
+                    MySqlCommand sqlCmd = new MySqlCommand(strSql, db);
+                    sqlCmd.Parameters.AddWithValue("@account", account);
+                    sqlCmd.Parameters.AddWithValue("@password", password);
+
+                    using (MySqlDataReader reader = sqlCmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }          
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                finally
+                {
+                    db.Close();
+                }
+                return false;
+            }
+        }
+
+        public DataTable GetData()
+        {
+            using (MySqlConnection db = new MySqlConnection("server=localhost; port=3306; uid=root; database=cuisinevision; charset=utf8;"))
+            {
+                try
+                {
+                    db.Open();
+                    String strCmd = "SELECT Primary_Key AS Number, Text_Entry AS Name FROM MyTable;";
+                    MySqlCommand cmdSelect = new MySqlCommand(strCmd, db);
+                    cmdSelect.ExecuteNonQuery();
+
+                    MySqlDataAdapter dataAdp = new MySqlDataAdapter(cmdSelect);
+                    DataTable dt = new DataTable();
+                    dataAdp.Fill(dt);
+                    db.Close();
+                    return dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    throw;
+                }
+                finally
+                {
+                    db.Close();
+                }
+            }
+        }
+    }
+}
---
+++ CuisineVision/FrmMain.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CuisineVision
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataAccess dta = new DataAccess();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (dta.DbLogin(txtAccount.Text.Trim(), txtPassword.Password))
+                {
+                    MessageBox.Show("Login Successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
